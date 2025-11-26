f9d9a754b100ef1468ea0b26a5122fe6624c0451
Arjun Kumar Seth <arjunreddy8182@gmail.com>
2025-11-26 18:13:03 +0530

    new schema

--- src/model/user.js
+++ src/model/user.js
@@ -3,22 +3,41 @@ const mogoose = require("mongoose")
 const userSchema = new mogoose.Schema({
     firstNmae : {
         type : String,
+        require : true,
+        mixlength : 4,
+        maxlength : 20
     },
     lastName : {
         type : String, 
     },
     emailID : {
-        type : String, 
+        type : String,
+        require : true,
+        unique : true,
+        validate(value){
+            if (!(value.includes("@"))){
+                throw new Error("plz enter correct email address")
+            }
+        } 
     },
     password : {
         type : String, 
     },
     age : {
         type : Number, 
+        min : 18
     },
     gender : {
         type : String,
+        validate(value){
+            if(!([male,female,other].includes(value))){
+                throw new Error("plz give correct email address")
+            }
+        }
     }
+},
+{
+    timestamps : True
 })
 
 module.exports = mogoose.model("user",userSchema) 
